f385f8ae45d1a356ecb0482bde67b466f4641f08
Arm Abramyan <aabramya@gmail.com>
2019-05-24 00:42:46 +0200

    All AnswerTypes bug fixes

--- src/app/content/exam/exam.component.ts
+++ src/app/content/exam/exam.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { APIService } from '../../services/api.service';
 import { Question, AnswerType } from '../../models/quiz.model';
-import { FormControl, FormControlName } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators, NgForm } from '@angular/forms';
+// import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+
+
 
 
 @Component({
@@ -18,22 +21,59 @@ export class ExamComponent implements OnInit {
   nbCorrectAnswers = 0;
   nbAnswered = 0;
   allAnswerTypes: AnswerType[] = [];
-
+  labels = ['ՃԻՇՏ Է', 'ՍԽԱԼ Է', 'ՉԳԻՏԵՄ'];
   answerTypes = [];
   @Input() quizId: number;
   @Input() quizThemeIDs: string = "";
   @Input() examStarted: boolean;
   examFinished: boolean = false;
+  @ViewChild('form') public answerForm: NgForm;
+
+  public myformGroup = new FormGroup({
+    "rows": new FormArray([
+      new FormArray([new FormControl(), new FormControl(), new FormControl()]),
+      new FormArray([new FormControl(), new FormControl(), new FormControl()]),
+      new FormArray([new FormControl(), new FormControl(), new FormControl()])
+    ])
+  });
+  
+
+//   <div[formGroup]="myGroup" >
+
+//   <div * ngFor="let city of cityArray.controls; index as i" >
+//     <input[formControlName]="i" >
+//       </div>
+//       < /div>
+//       < /div>
+
+// In your class:
+
+// this.cityArray = new FormArray([new FormControl('SF')]);
+// this.myGroup = new FormGroup({
+//   cities: this.cityArray
+// });
+
+  // for(const control of formArray.controls) {
+  //   if (control instanceof FormControl) {
+  //     console.log("Control is FormControl")
+  //   }
+  //   if (control instanceof FormGroup) {
+  //     console.log("Control is FormGroup");
+  //   }
+  //   if (control instanceof FormArray) {
+  //     console.log("control is FormArray")
+  //   }
+  // }
 
   constructor(
-    private api: APIService
+    private api: APIService,
   ) {
-    
+    console.log("myformGroup", this.myformGroup);
+
     /* TODO: Here should be api call to getAllAnswerTypes, hardcodding for now */
     this.api.getAllAnswerTypes().subscribe(
       (data) => {
-        this.allAnswerTypes = data;
-        
+        this.allAnswerTypes = data || [];
         this.allAnswerTypes.map((el,i) => {
           switch (i) {
             case 0: // ԸՆՏՐՈՎԻ ՊԱՏԱՍԽԱՆՈՂ ԱՌԱՋԱԴՐԱՆՔՆԵՐ
@@ -73,6 +113,14 @@ export class ExamComponent implements OnInit {
           }
         });
         this.resetAnswers();
+        /*this.answerTypes = [
+          // this.makeArray(this.allAnswerTypes[0].description.count),
+          [false, false, false, false],
+          "",
+          // this.makeArray(this.allAnswerTypes[2].description.count),
+          [false, false, false, false, false, false],
+          this.makeArray(this.allAnswerTypes[3].description.count, false, this.allAnswerTypes[3].description.rows)
+        ];*/
       }
     )
   }
@@ -93,43 +141,43 @@ export class ExamComponent implements OnInit {
               "questionText": "During development, you typically use the ng serve command to build, watch, and serve the application from local memory, using webpack-dev-server. When you are ready to deploy, however, you must use the ng build command to build the app and deploy the build artifacts elsewhere."
             },
             {
-              "id": 17, "quizID": 5, "quizThemeID": 7, "questionTypeID": 11, "answerTypeID": 12,
+              "id": 16, "quizID": 5, "quizThemeID": 7, "questionTypeID": 11, "answerTypeID": 12,
               "quizName": "Հայոց լեզու և հայ գրականություն",
               "quizThemeName": "Հնչյունաբանություն",
               "questionText": "This section covers changes you may have make to the server or to files deployed to the server."
             },
             {
-              "id": 18, "quizID": 5, "quizThemeID": 7, "questionTypeID": 11, "answerTypeID": 12,
+              "id": 17, "quizID": 5, "quizThemeID": 7, "questionTypeID": 11, "answerTypeID": 12,
               "quizName": "Հայոց լեզու և հայ գրականություն",
               "quizThemeName": "Հնչյունաբանություն",
               "questionText": "A routed application should support \"deep links\". A deep link is a URL that specifies a path to a component inside the app. For example, http://www.mysite.com/heroes/42 is a deep link to the hero detail page that displays the hero with id: 42."
             },
             {
-              "id": 19, "quizID": 5, "quizThemeID": 7, "questionTypeID": 12, "answerTypeID": 13,
+              "id": 18, "quizID": 5, "quizThemeID": 7, "questionTypeID": 12, "answerTypeID": 13,
               "quizName": "Հայոց լեզու և հայ գրականություն",
               "quizThemeName": "Հնչյունաբանություն",
               "questionText": "A static server routinely returns index.html when it receives a request for http://www.mysite.com/. But it rejects http://www.mysite.com/heroes/42 and returns a 404 - Not Found error unless it is configured to return index.html instead."
             },
             {
-              "id": 20, "quizID": 5, "quizThemeID": 7, "questionTypeID": 12, "answerTypeID": 13,
+              "id": 19, "quizID": 5, "quizThemeID": 7, "questionTypeID": 12, "answerTypeID": 13,
               "quizName": "Հայոց լեզու և հայ գրականություն",
               "quizThemeName": "Հնչյունաբանություն",
               "questionText":   "Production optimizations The --prod meta - flag engages the following build optimization features."
             },
             {
-              "id": 21, "quizID": 5, "quizThemeID": 7, "questionTypeID": 13, "answerTypeID": 14,
+              "id": 20, "quizID": 5, "quizThemeID": 7, "questionTypeID": 13, "answerTypeID": 14,
               "quizName": "Հայոց լեզու և հայ գրականություն",
               "quizThemeName": "Հնչյունաբանություն",
               "questionText": "0"
             },
             {
-              "id": 22, "quizID": 5, "quizThemeID": 7, "questionTypeID": 13, "answerTypeID": 14,
+              "id": 21, "quizID": 5, "quizThemeID": 7, "questionTypeID": 13, "answerTypeID": 14,
               "quizName": "Հայոց լեզու և հայ գրականություն",
               "quizThemeName": "Հնչյունաբանություն",
               "questionText": "GitHub Pages: you can't directly configure the GitHub Pages server, but you can add a 404 page. Copy index.html into 404.html. It will still be served as the 404 response, but the browser will process that page and load the app properly. It's also a good idea to serve from docs/ on master and to create a .nojekyll file Firebase hosting: add a rewrite rule."
             },
             {
-              "id": 23, "quizID": 5, "quizThemeID": 7, "questionTypeID": 10, "answerTypeID": 11,
+              "id": 22, "quizID": 5, "quizThemeID": 7, "questionTypeID": 10, "answerTypeID": 11,
               "quizName": "Հայոց լեզու և հայ գրականություն",
               "quizThemeName": "Հնչյունաբանություն",
               "questionText": "sss"
@@ -143,79 +191,87 @@ export class ExamComponent implements OnInit {
   nextQuestion(){
     if(this.curQuest < this.questions.length-1) {
       // Checking if the answer is confirmed
-      if (this.examResults[this.questions[this.curQuest].id]){
-        // this.answerTypes[0] = [false, false, false, false];
+     // if (this.examResults[this.questions[this.curQuest].id]){
         this.resetAnswers();
         this.curQuest++;
         /* TODO: This is an hardcode, should be replaced */
-      
-        this.curAnsType = Math.floor(Math.random() * Math.floor(3));
-        console.log(this.curAnsType );
-      } else {
-        // If question anser is not confirmed, we can't go forward
-      }
-      
+        this.curAnsType = Math.floor(Math.random() * Math.floor(4));
+        console.log("curAnsType = ", this.curAnsType, "answerTypes = ", this.answerTypes );
+      // } else {
+      //   // If question anser is not confirmed, we can't go forward
+      // }
     }
   }
 
-  selectAnswer(ev, answer:number, row:number = 0, answerType: number = 0) {
-    this.answerTypes[answerType][answer] = ev.target.checked;
+  selectAnswer(ev, answer: number, answerType: number = 0, row: number = undefined) {
+    if(row != undefined){
+      this.answerTypes[answerType][row][answer] = ev.target.checked;
+    } else {
+      this.answerTypes[answerType][answer] = ev.target.checked;
+    }
   }
 
   confirmAnswer(answerId: number) {
     if (!this.examResults[this.questions[this.curQuest].id]) {
       let answer: any = '';
-      switch (this.curAnsType) {
-        //case 0:
-        // Same as default case  
-        //break;
-        case 1:
-          answer = this.answerTypes[this.curAnsType];
-          break;
-        //case 2:
-        // Same as default case
-        //  break;
-        case 3:
-
-          break;
-        default:
-          answer = this.answerTypes[this.curAnsType].map((x, i) => { 
-            if (x) return i+1; 
+      if (this.curAnsType == 1){
+        answer = this.answerTypes[this.curAnsType];
+      } else {
+        answer = this.answerTypes[this.curAnsType].map((rowArr, row) => {
+          return this.answerTypes[this.curAnsType][row].map((x, i) => {
+            if (x) return i + 1;
           }).filter(x => x != undefined).join();
-          break;
-      }
-      this.examResults[this.questions[this.curQuest].id] = answer;
-      
-      this.nbAnswered++;
-      this.api.isAnswerCorrect(this.questions[this.curQuest].id).subscribe(
-        (data) => {
           
-          /* TODO: should be fixed when the back-end part is ready 
-          this.currentAnswer = data;*/
-          this.currentAnswer = Math.random() >= 0.5;
-          console.log(this.currentAnswer);
-          if (this.currentAnswer) {
-            this.nbCorrectAnswers++;
-          }
-        }
-      );
-      
-    
+        });
+      }
+
+      if ((typeof answer == "object" && answer.length > 1) || typeof answer != "object" ) {
+        this.examResults[this.questions[this.curQuest].id] = answer;
+      } else {
+        this.examResults[this.questions[this.curQuest].id] = answer[0];
+      }
+
+
+      this.nbAnswered++;
+
+      // this.api.isAnswerCorrect(this.questions[this.curQuest].id).subscribe(
+      //   (data) => {
+
+      //     /* TODO: should be fixed when the back-end part is ready 
+      //     this.currentAnswer = data;*/
+      //     this.currentAnswer = Math.random() >= 0.5;
+      //     if (this.currentAnswer && this.curQuest < this.questions.length - 1) {
+      //       this.nbCorrectAnswers++;
+      //     }
+      //   }
+      // );
+      this.currentAnswer = Math.random() >= 0.5;
+      if (this.currentAnswer && this.curQuest < this.questions.length - 1) {
+        this.nbCorrectAnswers++;
+      }
     } else {
       // The question is already answered, someone is trying to cheat
     }
-    
   }
 
-  makeArray(n:number = 0, value:any = false){
-    if(value == 'range'){
-      return Array.from(Array(n).keys())
+  makeArray(n:number = 0, value:any = false, rows:number = undefined){
+    let finalArray = [];
+    let fakeRow = rows || 1;
+
+    for (let row = 0; row < fakeRow; row++){
+      if (value == 'range') {
+        finalArray.push(Array.from(Array(n).keys()))
+      } else {
+        finalArray.push(Array(n).fill(value, 0));
+      }
+    }
+    if (rows == undefined){
+      return finalArray[0];
     } else {
-      return Array(n).fill(value, 0);
+      return finalArray;
     }
   }
   
-
   saveAnser(answerId: number) {
     console.log(answerId);
   }
@@ -227,16 +283,19 @@ export class ExamComponent implements OnInit {
     this.examResults = {};
     this.curQuest = 0;
     this.nbAnswered = 0;
-    
-
+    this.nbCorrectAnswers = 0;
+    this.resetAnswers();
   }
 
+ // (n: number = 0, value: any = false, rows: number = 1)
   resetAnswers() {
     this.answerTypes = [
-      this.makeArray(this.allAnswerTypes[0].description.count),
+      // this.makeArray(this.allAnswerTypes[0].description.count),
+      this.makeArray(this.allAnswerTypes[0].description.count, false, this.allAnswerTypes[0].description.rows),
       "",
-      this.makeArray(this.allAnswerTypes[2].description.count),
-      []
+     // [this.makeArray(this.allAnswerTypes[2].description.count)],
+      this.makeArray(this.allAnswerTypes[2].description.count, false, this.allAnswerTypes[2].description.rows),
+      this.makeArray(this.allAnswerTypes[3].description.count,false,this.allAnswerTypes[3].description.rows)
     ];
   }
 
